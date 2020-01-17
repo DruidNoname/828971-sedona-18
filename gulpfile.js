291d5deee6dd13f93bf1c3552a3acf9410556c28
Olga Stepanova <stepanova@elcode.ru>
2020-01-17 09:48:08 +0300

    Add webp

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,7 @@ var sass = require("gulp-sass");
 var csso = require("gulp-csso");
 var rename = require("gulp-rename");
 var imagemin = require("gulp-imagemin");
+var webp = require("gulp-webp");
 var htmlmin = require("gulp-htmlmin");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
@@ -68,6 +69,12 @@ gulp.task("images", function (){
     .pipe(gulp.dest("build/img"));
 });
 
+gulp.task("webp", function () {
+    return gulp.src("source/img/**/*.{png,jpg}")
+        .pipe(webp({quality: 90}))
+        .pipe(gulp.dest("build/img"));
+});
+
 gulp.task("html", function () {
   return gulp.src("source/*.html")
     .pipe(posthtml([
@@ -104,6 +111,7 @@ gulp.task("build", gulp.series(
   "css",
   "images",
   "sprite",
+  "webp",
   "html",
   "minify-html",
   "uglify"
